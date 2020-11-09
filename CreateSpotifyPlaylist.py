815a744ae7d30ae44773ff7e9fc7cd2fe46ba7ab
Alma Alvarado <47694944+almaavocado@users.noreply.github.com>
2020-11-08 20:31:03 -0800

    initial commit

---
+++ CreateSpotifyPlaylist.py
@@ -0,0 +1,27 @@
+'''''''''
+Step 1: Log into YouTube
+Step 2: Make sure we grab "Liked Videos"
+Step 3: Create a New Playlist for Liked Videos
+Step 4: Search the song in Spotify
+Step 5: Add the song to the playlist
+'''''
+
+class CreateSpotifyPlaylist:
+
+    def __init__(self):
+        pass
+
+    def get_youtube_client(self):
+        pass
+
+    def getliked_videos(selfs):
+        pass
+
+    def createPlaylist(selfs):
+        pass
+
+    def getSpotify_URL(self):
+        pass
+
+    def addSongtoPlaylist(self):
+        pass
